e46a5d4cf14aba4cbcb306f9e94dcebb0eb4447a
Matthew Trost <mrtrost@gmail.com>
2023-02-17 10:52:01 -0500

    rm fs dep

--- lib/index.ts
+++ lib/index.ts
@@ -1,54 +1,16 @@
-/**
- * @fileoverview Main entrypoint for libraries using yargs-parser in Node.js
- * CJS and ESM environments.
- *
- * @license
- * Copyright (c) 2016, Contributors
- * SPDX-License-Identifier: ISC
- */
-
-import { format } from 'util'
-import { normalize, resolve } from 'path'
 import { ArgsInput, Arguments, Parser, Options, DetailedArguments } from './yargs-parser-types.js'
 import { camelCase, decamelize, looksLikeNumber } from './string-utils.js'
 import { YargsParser } from './yargs-parser.js'
-import { readFileSync } from 'fs'
 
-// See https://github.com/yargs/yargs-parser#supported-nodejs-versions for our
-// version support policy. The YARGS_MIN_NODE_VERSION is used for testing only.
-const minNodeVersion = (process && process.env && process.env.YARGS_MIN_NODE_VERSION)
-  ? Number(process.env.YARGS_MIN_NODE_VERSION)
-  : 12
-const nodeVersion = process?.versions?.node ?? process?.version?.slice(1)
-if (nodeVersion) {
-  const major = Number(nodeVersion.match(/^([^.]+)/)![1])
-  if (major < minNodeVersion) {
-    throw Error(`yargs parser supports a minimum Node.js version of ${minNodeVersion}. Read our version support policy: https://github.com/yargs/yargs-parser#supported-nodejs-versions`)
-  }
-}
-
-// Creates a yargs-parser instance using Node.js standard libraries:
-const env = process ? process.env as { [key: string]: string } : {}
 const parser = new YargsParser({
-  cwd: process.cwd,
+  cwd: () => '',
   env: () => {
-    return env
+    return {}
   },
-  format,
-  normalize,
-  resolve,
-  // TODO: figure  out a  way to combine ESM and CJS coverage, such  that
-  // we can exercise all the lines below:
-  require: (path: string) => {
-    if (typeof require !== 'undefined') {
-      return require(path)
-    } else if (path.match(/\.json$/)) {
-      // Addresses: https://github.com/yargs/yargs/issues/2040
-      return JSON.parse(readFileSync(path, 'utf8'))
-    } else {
-      throw Error('only .json config files are supported in ESM')
-    }
-  }
+  format: () => '',
+  normalize: () => '',
+  resolve: () => '',
+  require: () => ''
 })
 const yargsParser: Parser = function Parser (args: ArgsInput, opts?: Partial<Options>): Arguments {
   const result = parser.parse(args.slice(), opts)
